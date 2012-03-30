2c5544335f4234d0e05448162f44dcabec387670
Damien MARTIN <damien@martin-damien.fr>
2012-03-30 20:52:36 +0200

    Pictures are now downloaded

--- chan.js
+++ chan.js
@@ -5,20 +5,58 @@ var exec    = require('child_process').exec;
 var spawn   = require('child_process').spawn;
 var $       = require('jquery');
 
-module.exports = {
+// This rule is for 4chan
+dom_elements = 'form a[target="_blank"] img';
+chan = null;
+storage_folder = './downloads';
 
-    storage_folder: './downloads',
+module.exports = {
 
     loadPage: function( html_url ){
 
+        chan = this;
+
         console.log("Loading " + html_url + "...");
-        this.makeFolder( this.storage_folder );
+        this.makeFolder( storage_folder );
+
+        var html = '';
+
+        var options = {
+
+            host: url.parse( html_url ).host,
+            port: 80,
+            path: url.parse( html_url ).pathname
+
+        }
+
+        // Load and read file
+        http.get(options, function(res){
+
+            res.on('data', function(data){
+
+                html += data;
+
+            }).on('end', function(){
+
+                // search for elements to download
+                $(html).find(dom_elements).each(function(){
+
+                    chan.downloadImage( $(this).parent().attr('href') );
+
+                });
+
+            });
+
+        });
 
     },
 
     downloadImage: function( img_url ){
 
-        // Download de fichier par : http://www.hacksparrow.com/using-node-js-to-download-files.html
+        // Download de fichier par :
+        // http://www.hacksparrow.com/using-node-js-to-download-files.html
+
+        img_url = 'http:' + img_url;
 
         var options = {
             host: url.parse( img_url ).host,
@@ -27,7 +65,7 @@ module.exports = {
         };
 
         var file_name = url.parse( img_url ).pathname.split( '/' ).pop();
-        var file = fs.createWriteStream( this.storage_folder + file_name );
+        var file = fs.createWriteStream( storage_folder + '/' + file_name );
 
         http.get( options, function( res ){
 
@@ -38,7 +76,6 @@ module.exports = {
             }).on('end', function(){
 
                 file.end();
-                console.log( file_name + ' downloaded to ' + this.storage_folder );
 
             });
 
